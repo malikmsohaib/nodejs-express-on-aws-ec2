4706c78d64588dc819c7d3cb319bc1a978d149e4
malikmsohaib <sobimalik9@gmail.com>
2024-02-01 02:08:28 +0500

    fixed h4 color

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1 style="background-color:powderblue;">Express Demo App</h1>  <h4 style="background-color:red;">Message: Success</h4> <p>Version 1.1</p>');
+  res.send('<h1 style="background-color:powderblue;">Express Demo App</h1>  <h2 style="color:blue;">Congratulations!!</h1> <h4 style="color:red;">Message: Success</h4> <p>Version 1.2</p>');
 })
 
 app.get('/products', (req, res) => {
@@ -17,7 +17,7 @@ app.get('/products', (req, res) => {
       price: 150
     }
   ])
-  
+
 })
 
 app.listen(port, ()=> {
