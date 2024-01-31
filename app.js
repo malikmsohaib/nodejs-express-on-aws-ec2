8f7af880af24774f086caf8dae38c8ce348f7dd9
malikmsohaib <sobimalik9@gmail.com>
2024-02-01 02:13:48 +0500

    fixed text align

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1 style="background-color:powderblue;">Express Demo App</h1>  <h2 style="color:blue;">Congratulations!!</h1> <h4 style="color:red;">Message: Success</h4> <p style="color:green;">Version 1.2</p>');
+  res.send('<body text-align: center;> <h1 style="background-color:powderblue;">Express Demo App</h1>  <h2 style="color:blue;">Congratulations!!</h1> <h4 style="color:red;">Message: Success</h4> <p style="color:green;">Version 1.2</p> </body>');
 })
 
 app.get('/products', (req, res) => {
